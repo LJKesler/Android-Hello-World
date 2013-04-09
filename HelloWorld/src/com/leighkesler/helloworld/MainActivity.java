279efead7a64fef2f107e06021866e3b3f732bea
LJKesler <ljkesler@gmail.com>
2013-04-09 10:23:03 -0400

    Updates

--- HelloWorld/src/com/leighkesler/helloworld/MainActivity.java
+++ HelloWorld/src/com/leighkesler/helloworld/MainActivity.java
@@ -1,11 +1,24 @@
 package com.leighkesler.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.leighkesler.helloworld.MESSAGE";
+	
+	public void sendMessage(View view){
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText)findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
